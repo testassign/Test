071046a16573757cbe78812ec5955130440aaab9
testassign <ufaro361@gmail.com>
2021-04-08 23:01:12 -0400

    first commit

---
+++ Simple.java
@@ -0,0 +1,36 @@
+
+public class Simple {
+
+	public static void main(String[] args) {
+		System.out.println("Programming is fun!");
+	}
+}
+
+// Java source code files end with .java
+
+/*
+public class simple ==> class header
+	-> one of the use of a class is to serve as a container 
+	   for an application
+	
+	-> public is known as an access specifier
+	   -> public means access to the class is unrestricted 
+	   
+	-> class is a Java key word
+	
+	-> Simple is the class name
+	
+	
+Note: You may create more than one class in a file, but you may 
+have only one public class per Java file.
+
+*/
+
+/*
+public static void main(String[] args) ===> known as method header
+
+A method can be thought of as a group of one or more programming
+statements that collectively has a name
+
+main is the method name ==? stating point of an application
+*/
